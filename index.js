d76052a422ee8d0b57e9328c175e6d3d70e0cbb5
NickAnaya618 <anaya.nicholas@gmail.com>
2017-03-16 00:43:11 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(icecream_name) {
+  return `I love ${icecream_name}`;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase()
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
